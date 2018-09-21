2933ea5226a28166e5cd939268cd66b506b1f6dd
Theophelus <aneletom10@gmail.com>
2018-09-21 16:15:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+// const flash = require('express-flash');
+// const session = require('express-session');
+
+// const pg = require("pg");
+// const Pool = pg.Pool;
+
+//define instances
+let app = express();
+
+// initialise session middleware - flash-express depends on it
+// app.use(session({
+//     secret: "<add a secret string here>",
+//     resave: false,
+//     saveUninitialized: true
+// }));
+// // initialise the flash middleware
+// app.use(flash());
+
+// let local = process.env.LOCAL || false;
+// if (process.env.DATABASE_URL && !local) {
+//     useSSL = true;
+// }
+// //define a connection string to be able to connect to the database.
+// const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/registration_numberDB';
+// const pool = new Pool({
+//     connectionString
+//     // ssl: useSSL
+// });
+// define instance of factory function
+
+// configure express handlebars
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+// parse application/json
+app.use(bodyParser.json());
+//configure public for=lder for static files
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render('home')
+});
+
+let PORT = process.env.PORT || 3020;
+app.listen(PORT, () => {
+    console.log('app starting on PORT', PORT);
+});
